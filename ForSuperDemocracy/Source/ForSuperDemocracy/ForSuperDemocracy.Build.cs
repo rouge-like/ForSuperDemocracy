999bec3746357688c453217994808db8c844511d
Huxley <blanche.sight@gmail.com>
2025-09-09 15:40:16 +0900

    Update: AI Module Update
    0. Terminid AI 용 모듈 추가
    1. Rootpath 에러 가끔 나길래 Module 디렉토리부터 path잡도록 세팅해둠

--- ForSuperDemocracy/Source/ForSuperDemocracy/ForSuperDemocracy.Build.cs
+++ ForSuperDemocracy/Source/ForSuperDemocracy/ForSuperDemocracy.Build.cs
@@ -9,8 +9,19 @@ public class ForSuperDemocracy : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		
+		
+		// Terminid AI용 모듈
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"StateTreeModule", "StateTreeEditorModule", "StructUtils", "PropertyPath", "EnvironmentQueryEditor", "AIModule", "NavigationSystem", "GameplayTasks"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		
+		// ModuleDirectory path로 잡도록 추가 (Rider에러 방지)
+		PublicIncludePaths.Add(ModuleDirectory);
+		PrivateIncludePaths.Add(ModuleDirectory);
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
