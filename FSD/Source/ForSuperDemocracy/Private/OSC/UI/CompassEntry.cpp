b99de12f63141da867416a7574df9ccb45964667
rouge-like <chanipm29@gmail.com>
2025-09-21 22:40:38 +0900

    20250921 Compass UI + BGM

---
+++ FSD/Source/ForSuperDemocracy/Private/OSC/UI/CompassEntry.cpp
@@ -0,0 +1,16 @@
+﻿#include "OSC/UI/CompassEntry.h"
+
+#include "Components/TextBlock.h"
+
+
+void UCompassEntry::SetDistanceText(const float Distance) const
+{
+	FString text = FString::FromInt(Distance / 100) + "m";
+	DistanceText->SetText(FText::FromString(text));
+}
+
+void UCompassEntry::SetIndexText(const int32 Index) const
+{
+	FString text = "[" + FString::FromInt(Index + 1) + "]";
+	IndexText->SetText(FText::FromString(text));
+}
---
+++ FSD/Source/ForSuperDemocracy/Public/OSC/UI/CompassEntry.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CompassEntry.generated.h"
+
+class UImage;
+class UTextBlock;
+
+UCLASS()
+class FORSUPERDEMOCRACY_API UCompassEntry : public UUserWidget
+{
+    GENERATED_BODY()
+public:
+    void SetDistanceText(const float Distance) const;
+    void SetIndexText(const int32 Index) const;
+
+protected:
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget), Category = "Compass")
+    TObjectPtr<UTextBlock> DistanceText;
+    
+    UPROPERTY(BlueprintReadOnly, meta = (BindWidget), Category = "Compass")
+    TObjectPtr<UTextBlock> IndexText;
+};
